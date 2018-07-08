950c43af10a2921165a8cdab5f2579fade259d74
Yashraj Mishra <mishra.yashrajs@outlook.com>
2018-07-08 16:30:54 +0530

    Final

--- app/src/main/java/com/amitelectric/yashraj/amrdeviceconfiguration/MainActivity.java
+++ app/src/main/java/com/amitelectric/yashraj/amrdeviceconfiguration/MainActivity.java
@@ -1,10 +1,12 @@
 package com.amitelectric.yashraj.amrdeviceconfiguration;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -65,10 +67,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-              Uri uri = Uri.parse("smsto:" + Number);
+
+
+
+
+              /*Uri uri = Uri.parse("smsto:" + Number);
               Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
               intent.putExtra("sms_body", Message);
-              startActivity(intent);
+              startActivity(intent);*/
+
+
+              SmsManager smsManager = SmsManager.getDefault();
+              smsManager.sendTextMessage(Number, null, Message, null, null);
+
+              AlertDialog.Builder alertDialogBuilder  = new AlertDialog.Builder(MainActivity.this);
+              alertDialogBuilder.setMessage("Command Sent");
+              alertDialogBuilder.show();
 
 
 
--- app/src/main/java/com/amitelectric/yashraj/amrdeviceconfiguration/ResetTotalizer.java
+++ app/src/main/java/com/amitelectric/yashraj/amrdeviceconfiguration/ResetTotalizer.java
@@ -1,9 +1,11 @@
 package com.amitelectric.yashraj.amrdeviceconfiguration;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,12 +33,18 @@ public class ResetTotalizer extends AppCompatActivity {
                 String DeviceNumber1 = editText2.getText().toString();
                 String Message = "84911103:RESET TOTAL=" + TotalizeerValue + "=";
 
-
-                Uri uri = Uri.parse("smsto:" + DeviceNumber1);
+/* Uri uri = Uri.parse("smsto:" + DeviceNumber1);
                 Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
                 intent.putExtra("sms_body", Message);
-                startActivity(intent);
+                startActivity(intent);*/
 
+
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(DeviceNumber1, null, Message, null, null);
+
+                AlertDialog.Builder alertDialogBuilder  = new AlertDialog.Builder(ResetTotalizer.this);
+                alertDialogBuilder.setMessage("Command Sent");
+                alertDialogBuilder.show();
 
 
             }
